82ddb07e533d519b8052ed7b56f37770a568ee8a
Josh Pewtress <61856857+JoshPewtress@users.noreply.github.com>
2025-10-22 15:13:35 -0600

    Moved Helper Class to a Code Behind File

---
+++ CalendarDarts/CalendarDartsApp/Helper.cs
@@ -0,0 +1,43 @@
+﻿static class Helper
+{
+    public static string PlayGame()
+    {
+        var targetDate = SetTargetDate();
+        var userGuess = GuessAmountOfDays(targetDate);
+        var dayDifference = GetDateDifferenceFromUsersGuess(targetDate, userGuess);
+        return DisplayResults(dayDifference, targetDate, userGuess);
+    }
+
+    private static string DisplayResults(int dayDifference, DateOnly targetDate, int userGuess)
+    {
+        var dateDifference = $"The difference between {targetDate} and today is {GetDateDifferenceFromTargetAndToday(targetDate)} days.\n";
+
+        return dayDifference >= -5 && dayDifference <= 5
+                ? $"You won! {dateDifference}"
+                : $"Sorry, You lost. {dateDifference}";
+    }
+
+    private static int GetDateDifferenceFromUsersGuess(DateOnly targetDate, int userGuess) =>
+        targetDate.DayNumber - DateOnly.FromDateTime(DateTime.Today.AddDays(userGuess)).DayNumber;
+
+    private static int GetDateDifferenceFromTargetAndToday(DateOnly targetDate) =>
+        targetDate.DayNumber - DateOnly.FromDateTime(DateTime.Today).DayNumber;
+
+    private static int GuessAmountOfDays(DateOnly targetDate)
+    {
+        int output;
+        bool validInput = false;
+
+        do
+        {
+            Console.Write($"Amount of days between {targetDate} and today: ");
+
+            validInput = (int.TryParse(Console.ReadLine(), out output) && output > 0);
+        } while (validInput == false);
+
+        return output;
+    }
+
+    private static DateOnly SetTargetDate() =>
+        DateOnly.FromDateTime(DateTime.Now).AddDays(Random.Shared.Next(1, 366));
+}
