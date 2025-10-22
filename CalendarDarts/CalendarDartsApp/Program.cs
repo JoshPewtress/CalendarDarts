d6e809fcb22da051ebf9a8a70109c31dfa949fcd
Josh Pewtress <61856857+JoshPewtress@users.noreply.github.com>
2025-10-22 14:08:46 -0600

    Add GuessAmountOfDays Method
    Asks for and validates user input.

--- CalendarDarts/CalendarDartsApp/Program.cs
+++ CalendarDarts/CalendarDartsApp/Program.cs
@@ -1,7 +1,33 @@
-﻿
+﻿var userGuess = Helper.PlayGame();
+
+Console.WriteLine(userGuess);
+
+Console.ReadLine();
 
 static class Helper
 {
+    public static int PlayGame()
+    {
+        var targetDate = SetTargetDate();
+        var userGuess = GuessAmountOfDays(targetDate);
+        return userGuess;
+    }
+
+    private static int GuessAmountOfDays(DateOnly targetDate)
+    {
+        int output;
+        bool validInput = false;
+
+        do
+        {
+            Console.Write($"Amount of days between {targetDate} and today: ");
+
+            validInput = (int.TryParse(Console.ReadLine(), out output) && output > 0);
+        } while (validInput == false);
+
+        return output;
+    }
+
     private static DateOnly SetTargetDate() =>
         DateOnly.FromDateTime(DateTime.Now).AddDays(Random.Shared.Next(1, 366));
 }
