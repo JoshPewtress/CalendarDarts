90ae3ee07045cc817ddcc2039b93312d0846008a
Josh Pewtress <61856857+JoshPewtress@users.noreply.github.com>
2025-10-23 16:42:58 -0600

    Refactor GetDateDifference methods to GetNewDate method - Add readonly variable TodaysDate - Remove SetTargetDate method - Randomly Generated int is passed to GetNewDate and stored - User Guess int is passed to GetNewDate and stored

--- CalendarDarts/CalendarDartsApp/Helper.cs
+++ CalendarDarts/CalendarDartsApp/Helper.cs
@@ -1,27 +1,31 @@
 ﻿static class Helper
 {
+    private static readonly DateOnly todaysDate = DateOnly.FromDateTime(DateTime.Now);
+
     public static string PlayGame()
     {
-        var targetDate = SetTargetDate();
-        var userGuess = GuessAmountOfDays(targetDate);
-        var dayDifference = GetDateDifferenceFromUsersGuess(targetDate, userGuess);
-        return DisplayResults(dayDifference, targetDate, userGuess);
+        int randomResult = Random.Shared.Next(1, 366);
+
+        DateOnly targetDate = GetNewDate(randomResult);
+        int userGuess = GuessAmountOfDays(targetDate);
+        DateOnly userDate = GetNewDate(userGuess);
+        return DisplayResults(randomResult, userGuess, targetDate, userDate);
     }
 
-    private static string DisplayResults(int dayDifference, DateOnly targetDate, int userGuess)
+    private static string DisplayResults(int randomResult, int userGuess, DateOnly targetDate, DateOnly userDate)
     {
-        var dateDifference = $"The difference between {targetDate} and today is {GetDateDifferenceFromTargetAndToday(targetDate)} days.\n";
+        var difference = Math.Abs(randomResult - userGuess);
 
-        return dayDifference >= -5 && dayDifference <= 5
+        var dateDifference = $"Your Guess of {userDate.ToString("MMMM dd, yyyy")} was {difference} " +
+            $"{((difference) <= 1 ? "day" : "days")} away from the correct date of {targetDate.ToString("MMMM dd, yyyy")}";
+
+        return difference <= 5
                 ? $"You won! {dateDifference}"
                 : $"Sorry, You lost. {dateDifference}";
     }
 
-    private static int GetDateDifferenceFromUsersGuess(DateOnly targetDate, int userGuess) =>
-        targetDate.DayNumber - DateOnly.FromDateTime(DateTime.Today.AddDays(userGuess)).DayNumber;
-
-    private static int GetDateDifferenceFromTargetAndToday(DateOnly targetDate) =>
-        targetDate.DayNumber - DateOnly.FromDateTime(DateTime.Today).DayNumber;
+    private static DateOnly GetNewDate(int daysToAdd) =>
+        todaysDate.AddDays(daysToAdd);
 
     private static int GuessAmountOfDays(DateOnly targetDate)
     {
@@ -37,7 +41,4 @@
 
         return output;
     }
-
-    private static DateOnly SetTargetDate() =>
-        DateOnly.FromDateTime(DateTime.Now).AddDays(Random.Shared.Next(1, 366));
 }
